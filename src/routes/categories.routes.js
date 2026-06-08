d60996facdc4fd61226328054e2e5e8d45feedbb
Dylan <dylan.gavilan32@gmail.com>
2026-06-08 13:50:20 -0300

    auth routes

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const categoryController = require("../controllers/category.controller");
 const validateRequest = require("../middlewares/validateRequest");
 const { cache, invalidateCache } = require("../middlewares/cache");
+const authenticate = require("../middlewares/authenticate");
+const authorize = require("../middlewares/authorize");
 const {
   categoryIdValidation,
   createCategoryValidation,
@@ -12,8 +14,8 @@ const router = Router();
 
 router.get("/", cache("categories", 120), categoryController.list);
 router.get("/:id", categoryIdValidation, validateRequest, cache("categories", 120), categoryController.get);
-router.post("/", createCategoryValidation, validateRequest, invalidateCache("categories"), categoryController.create);
-router.put("/:id", updateCategoryValidation, validateRequest, invalidateCache("categories"), categoryController.update);
-router.delete("/:id", categoryIdValidation, validateRequest, invalidateCache("categories"), categoryController.remove);
+router.post("/", authenticate, authorize("ADMIN"), createCategoryValidation, validateRequest, invalidateCache("categories"), categoryController.create);
+router.put("/:id", authenticate, authorize("ADMIN"), updateCategoryValidation, validateRequest, invalidateCache("categories"), categoryController.update);
+router.delete("/:id", authenticate, authorize("ADMIN"), categoryIdValidation, validateRequest, invalidateCache("categories"), categoryController.remove);
 
 module.exports = router;
--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const movieController = require("../controllers/movie.controller");
 const validateRequest = require("../middlewares/validateRequest");
+const authenticate = require("../middlewares/authenticate");
+const authorize = require("../middlewares/authorize");
 const {
   movieIdValidation,
   createMovieValidation,
@@ -11,8 +13,8 @@ const router = Router();
 
 router.get("/", movieController.list);
 router.get("/:id", movieIdValidation, validateRequest, movieController.get);
-router.post("/", createMovieValidation, validateRequest, movieController.create);
-router.put("/:id", updateMovieValidation, validateRequest, movieController.update);
-router.delete("/:id", movieIdValidation, validateRequest, movieController.remove);
+router.post("/", authenticate, authorize("ADMIN"), createMovieValidation, validateRequest, movieController.create);
+router.put("/:id", authenticate, authorize("ADMIN"), updateMovieValidation, validateRequest, movieController.update);
+router.delete("/:id", authenticate, authorize("ADMIN"), movieIdValidation, validateRequest, movieController.remove);
 
 module.exports = router;
